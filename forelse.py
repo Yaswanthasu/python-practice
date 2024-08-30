637fcc8345c2c3fabfacf71c349834239027ba2c
Yaswanthasu <cs23b1009@nitpy.ac.in>
2024-08-31 00:34:38 +0530

    forelse

---
+++ forelse.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    print(i)
+    # if i == 3:
+        # break
+else:
+    print('sorry no i')    
+
+i=0
+while i<7:
+    print(i)
+    i = i+1
+else:
+    print('sorry no i')
+for x in range(5):
+    print('iteration no {} in for loop'.format(x+1))        
+else:
+    print('else block in loop')
+print('out of loop')    
